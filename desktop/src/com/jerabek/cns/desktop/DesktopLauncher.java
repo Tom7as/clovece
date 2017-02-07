1e9e5b9a03a22b3ee3121bc41511b67ec6f9d883
TOMAS-PC\Tomas <t7s>
2017-02-07 15:02:38 +0100

    gdx

---
+++ desktop/src/com/jerabek/cns/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jerabek.cns.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jerabek.cns.CloveceNezlobSe;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CloveceNezlobSe(), config);
+	}
+}
---
+++ desktop/src/com/jerabek/cnz/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jerabek.cnz.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jerabek.cnz.CloveceNezlobSe;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CloveceNezlobSe(), config);
+	}
+}
